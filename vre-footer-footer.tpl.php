2f29555495a8470d358d6806819baf996174bc5c
rdrew <robdrewupei@gmail.com>
2015-05-07 15:29:06 -0300

    1st commit

---
+++ vre-footer-footer.tpl.php
@@ -0,0 +1,79 @@
+<div class="upei-footer">
+	<div class="footer-links">
+		<div class="clear-block clr" id="links-section">
+			
+			<div class="section bottom-links menu-01 bl-first">
+				<h2 class="title">Getting Started</h2>
+				<ul class="menu">
+					<li><a href="http://www.upei.ca/research">Research at UPEI</a></li>
+					<li><a href="http://www.upei.ca/research/research-services">Research Services</a></li>
+					<li><a href="http://www.upei.ca/research/notices">Research Notices</a></li>
+					<li><a href="http://www.upei.ca/research/research-staff">Research Contacts</a></li>
+				</ul>
+			</div>
+			
+			<div class="section bottom-links menu-02">
+				<h2 class="title">Funding &amp; Supports<a href="http://www.upei.ca/research" title="">​</a></h2>
+				<ul class="menu">
+					<li class="leaf"><a href="http://www.upei.ca/research/research-services/find-researcher" title="">Find a Researcher</a></li>
+					<li class="leaf"><a href="http://www.upei.ca/research/forms" title="">Research Forms</a></li>
+					<li class="leaf"><a href="http://www.nserc-crsng.gc.ca/International-Internationale/Index_eng.asp">NSERC</a>&nbsp;<a href=".">/</a>&nbsp;<a href="http://www.cihr-irsc.gc.ca/e/27171.html">CIHR</a>&nbsp;<a href=".">/</a>&nbsp;<a href="http://www.sshrc-crsh.gc.ca/funding-financement/index-eng.aspx">SSHRC</a></li>
+					<li class="leaf"><a href="http://www.genomecanada.ca/">Genome Canada</a>&nbsp;<a href=".">/</a>&nbsp;<a href="http://www.innovation.ca/">CFI</a></li>
+				</ul>
+			</div>
+			
+			<div class="section bottom-links menu-03">
+				<h2 class="title">Managing Research</h2>
+				<ul class="menu">
+					<li class="leaf"><a href="http://library.upei.ca/vre">Virtual Research Environments</a>&nbsp;</li>
+					<li class="leaf"><a href="http://library.upei.ca/vre/data">Data Management Planning</a></li>
+					<li class="leaf"><a href="http://www.upei.ca/itss/">ITSS</a> <a href=".">/</a> <a href="http://library.upei.ca">Library</a>&nbsp;</li>
+				</ul>
+			</div>
+			
+			<div class="section bottom-links menu-04">
+				<h2 class="title">Sharing Research</h2>
+				<ul class="menu">
+					<li class="leaf first"><a href="http://islandscholar.ca">IslandScholar.ca</a></li>
+					<li class="leaf first"><a href="http://files.upei.ca/research/upei_open_access_policy.pdf">UPEI Open Access Policy</a></li>
+					<li class="leaf first"><a href="http://www.carl-abrc.ca/en/scholarly-communications/resources-for-authors.html">CARL Author Resources</a></li>
+					<li class="leaf first"><a href="http://www.openoasis.org/">OASIS SourceBook</a></li>
+				</ul>
+			</div>
+			
+			<div class="section bottom-links menu-05">
+				<h2 class="title">Domain Resources</h2>
+				<ul class="menu">
+					<li class="leaf first"><a href="http://www.npdi-us.org">NPDI</a></li>
+					<li class="leaf"><a href="http://www.pharmacognosy.us/pharmacognosy-links/natural-products-news/">ASP Natural Products News</a></li>
+				</ul>
+			</div>
+			
+		</div>
+		<div class="footeraddress">
+			<div class="inner-footeraddress">
+				<div class="addressleft">
+					<div class="address">
+						550 University Avenue, Charlottetown<br>
+						Prince Edward Island, Canada C1A 4P3<br>
+					</div>
+					<div class="links-last">
+						<a href="http://www.upei.ca/emergency/">Emergency Preparedness</a> |
+						<a href="http://home.upei.ca/about-upei/publications">Publications</a> |
+						<a href="https://cab.upei.ca/privacy-committee">Privacy Policy</a> |
+						<a href="http://home.upei.ca/disclaimer">Disclaimer</a> |
+						<a href="http://home.upei.ca/about-website">About this Website</a> |
+						<a href="http://home.upei.ca/directory">Contact UPEI</a>&nbsp;&nbsp;&nbsp;
+					</div>
+				</div>
+				<div class="socialmedia">
+					<a class="noicon" href="http://facebook.com/UniversityofPEI"><img src="https://files.upei.ca/misc/social/upei_facebook.png"></a>
+					<a class="noicon" href="http://twitter.com/upei"><img src="https://files.upei.ca/misc/social/upei_twitter.png"></a>
+					<a class="noicon" href="http://youtube.com/UofPEI"><img src="https://files.upei.ca/misc/social/upei_youtube.png"></a>
+					<a class="noicon" href="http://instagram.com/upei"><img src="https://files.upei.ca/misc/social/upei_instagram.png"></a>
+				</div>
+				<div class="footerlogo"><img src="https://files.upei.ca/misc/shieldfooter.png"></div>
+			</div>
+		</div>
+		</div><!-- /footer-links -->
+	</div>
